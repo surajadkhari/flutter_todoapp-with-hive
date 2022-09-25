e3f0e36149efa6fe2dfa8e7c6b8446dea7b930f4
Surat Adhikari <surajadkhari@gmail.com>
2022-09-25 13:29:17 +0545

    commit

---
+++ lib/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "TO DO",
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: CheckboxListTile(
+                onChanged: (value) {},
+                value: true,
+                title: const Text("Learn flutter everyday"),
+              )
+              //  Row(
+              //   children: const [Text("Hello")],
+              // ),
+              )
+        ],
+      ),
+    );
+  }
+}
