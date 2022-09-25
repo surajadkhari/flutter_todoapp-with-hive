2e247a8bea0a77538279065c634d8c92c6f3c0af
Surat Adhikari <surajadkhari@gmail.com>
2022-09-25 14:06:38 +0545

    add onChangedmethod

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_with_hive/app/common/todo_card.dart';
+import 'package:todo_app_with_hive/model/task_model.dart';
 
-import 'app/common/todo_card.dart';
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<TaskModel> toDoList = [
+    TaskModel(
+      taskName: "Hlelo",
+    ),
+    TaskModel(
+      taskName: "Hlelo",
+    ),
+  ];
+  void checkBoxChanged({required bool value, required int index}) {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,19 +27,13 @@ class Home extends StatelessWidget {
           "TO DO",
         ),
       ),
-      body: ListView(
-        children: [
-          TodoCard(
-            isTaskcompleted: true,
-            onChanged: (value) {},
-            taskName: "Hllp",
-          ),
-          TodoCard(
-            isTaskcompleted: true,
-            onChanged: (value) {},
-            taskName: "Flutter Everday",
-          )
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) => TodoCard(
+            isTaskcompleted: toDoList[index].isTaskCompleted,
+            onChanged: (value) => checkBoxChanged(
+                index: index, value: toDoList[index].isTaskCompleted),
+            taskName: toDoList[index].taskName),
       ),
     );
   }
--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -1,5 +1,5 @@
 class TaskModel {
   final String taskName;
   final bool isTaskCompleted;
-  TaskModel({required this.taskName, required this.isTaskCompleted});
+  TaskModel({required this.taskName,  this.isTaskCompleted=false});
 }
