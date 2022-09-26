1a53a0aeb1906a85f70e23336252013afae3ff22
Surat Adhikari <surajadkhari@gmail.com>
2022-09-26 10:35:42 +0545

    define createinitalData,loadData and updateData methods

--- lib/data/local_db.dart
+++ lib/data/local_db.dart
@@ -3,15 +3,24 @@ import 'package:hive_flutter/adapters.dart';
 import '../model/task_model.dart';
 
 class LocalDb {
-  List<TaskModel> toDoList = [
-    TaskModel(
-      taskName: "Todo app",
-    ),
-    TaskModel(
-      taskName: "Add to cart",
-    ),
-    TaskModel(taskName: "E-commerce ", isTaskCompleted: true)
-  ];
+  List<TaskModel> toDoList = [];
   //reference our box
   final _myBox = Hive.openBox("taskLocalDb");
+
+  void createinitalData() {
+    toDoList = [
+      TaskModel(
+        taskName: "Todo app",
+      ),
+      TaskModel(
+        taskName: "Add to cart",
+      ),
+      TaskModel(taskName: "E-commerce ", isTaskCompleted: true)
+    ];
+  }
+
+  //load data from  Db
+  void loadData() {}
+  //update
+  void update() {}
 }
