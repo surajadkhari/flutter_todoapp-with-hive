d43500793808e0151cd783761490ad98656fee8b
Surat Adhikari <surajadkhari@gmail.com>
2022-09-27 17:13:37 +0545

    slidable visibility on checkbox

--- lib/common/todo_card.dart
+++ lib/common/todo_card.dart
@@ -16,40 +16,69 @@ class TodoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 14),
-      child: Slidable(
-        enabled: true,
-        endActionPane: ActionPane(motion: const StretchMotion(), children: [
-          SlidableAction(
-            onPressed: deleteFunction,
-            icon: Icons.delete,
-            borderRadius: BorderRadius.circular(10),
-            backgroundColor: Colors.red,
-          ),
-        ]),
-        child: Container(
-          padding: const EdgeInsets.all(24),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-          child: Row(
-            children: [
-              Checkbox(
-                value: isTaskcompleted,
-                onChanged: onChanged,
-                activeColor: Colors.green,
+    return isTaskcompleted == true
+        ? Padding(
+            padding:
+                const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 14),
+            child: Slidable(
+              enabled: true,
+              endActionPane:
+                  ActionPane(motion: const StretchMotion(), children: [
+                SlidableAction(
+                  onPressed: deleteFunction,
+                  icon: Icons.delete,
+                  borderRadius: BorderRadius.circular(10),
+                  backgroundColor: Colors.red,
+                ),
+              ]),
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: isTaskcompleted,
+                      onChanged: onChanged,
+                      activeColor: Colors.green,
+                    ),
+                    Text(
+                      taskName,
+                      style: TextStyle(
+                          decoration: isTaskcompleted
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none),
+                    )
+                  ],
+                ),
               ),
-              Text(
-                taskName,
-                style: TextStyle(
-                    decoration: isTaskcompleted
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          )
+        : Padding(
+            padding:
+                const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 14),
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: isTaskcompleted,
+                    onChanged: onChanged,
+                    activeColor: Colors.green,
+                  ),
+                  Text(
+                    taskName,
+                    style: TextStyle(
+                        decoration: isTaskcompleted
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
